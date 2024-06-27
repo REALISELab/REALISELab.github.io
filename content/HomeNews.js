7493e7216c15c089676011e9695781ce05d8c5aa
Diego Costa <diegoelias1@gmail.com>
2024-06-27 15:25:17 -0400

    Simplifying the news in the header page.

--- content/HomeNews.js
+++ content/HomeNews.js
@@ -1,20 +1,17 @@
 export const news = [
     {
         title: 'Our paper on "A Transformer-based Approach for Augmenting Software Engineering Chatbots Datasets" has been accepted at ESEM 2024! Pre-print coming soon.',
-    },
-    {
-        title: 'Zakaria has a poster presentation accepted at SEMLA on the topic of Bias Mitigation Methods',
-        description: 'Published on May 1, 2024 by Diego Elias Costa',
-        href:"/blog/frqnt-project"  
+        description: 'July, 2024'
+        href:"/blog/esem-2024"
     },
     {
         title: 'FRQNT has supported a 2-year reserach project to support open source library maintainers',
-        description: 'Published on May 1, 2024 by Diego Elias Costa',
+        description: 'May, 2024',
         href:"/blog/frqnt-project"    
     },
     {
         title: ' Rachna received the Best Student Presentation Award at MSR for her presentation on the work “The role of library versions in Developer-ChatGPT conversations”. Congratulations Rachna!',
-        description: 'May 2024 by Diego Elias Costa',
+        description: 'May, 2024',
         href:"/blog/mdx-blog-post"
     },
     
