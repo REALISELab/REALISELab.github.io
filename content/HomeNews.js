7762cc33f50ffdd99990564de36ad9b10326aa60
Diego Costa <diegoelias1@gmail.com>
2024-06-27 12:59:57 -0400

    Including recent news.

--- content/HomeNews.js
+++ content/HomeNews.js
@@ -1,5 +1,13 @@
 export const news = [
     {
+        title: 'Our paper on "A Transformer-based Approach for Augmenting Software Engineering Chatbots Datasets" has been accepted at ESEM 2024! Pre-print coming soon.',
+    },
+    {
+        title: 'Zakaria has a poster presentation accepted at SEMLA on the topic of Bias Mitigation Methods',
+        description: 'Published on May 1, 2024 by Diego Elias Costa',
+        href:"/blog/frqnt-project"  
+    },
+    {
         title: 'FRQNT has supported a 2-year reserach project to support open source library maintainers',
         description: 'Published on May 1, 2024 by Diego Elias Costa',
         href:"/blog/frqnt-project"    
