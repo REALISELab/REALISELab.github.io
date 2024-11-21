f526353c60b9db1eeb22deca31c9b3136420f1a6
MohamedBilelBesbes <mohamedbilel.besbes@supcom.tn>
2024-11-21 10:29:34 -0500

    Make recent post count in sidebar up to 10

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,12 +53,15 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+
+	
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-         
+          blogSidebarTitle: 'Recent Posts',
+          blogSidebarCount: 10,
+        
         },
+
         theme: {
           customCss: './src/css/custom.css',
           
