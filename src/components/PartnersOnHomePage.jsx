848ff09a29b2b5d969d8cc8b5745e5eb66eb2cf9
Mohamed Bilel Besbes <mohamedbilelbesbes@na-annihilus.encs.concordia.ca>
2025-04-29 16:47:29 -0400

    Attenpting fix for logos display

--- src/components/PartnersOnHomePage.jsx
+++ src/components/PartnersOnHomePage.jsx
@@ -2,41 +2,33 @@ import React from "react";
 import "tailwindcss/tailwind.css";
 import "../css/custom.css";
 import { partners } from "../../content/Partners";
-import { useColorMode } from "@docusaurus/theme-common";  // Docusaurus hook!
 
 const PartnersMainPage = () => {
-  const { colorMode } = useColorMode();  // 'light' or 'dark'
-
   return (
     <div className="mx-auto flex justify-center w-full">
       <div className="mx-auto mt-10 grid grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 lg:mx-0 w-full lg:grid-cols-3">
         {partners.map((partner) => (
-
-
-
           <article
             key={partner.name}
             className="flex w-full flex-col items-center justify-between"
           >
-            <div className="group relative w-full h-48 flex items-center justify-center rounded-xl transition hover:shadow-md dark:bg-gray-200">
+            <div className="group relative w-full h-48 rounded-xl transition hover:shadow-md dark:bg-gray-200 flex items-center justify-center">
               <a
                 href={partner.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="block w-full h-full flex items-center justify-center"
+                className="flex items-center justify-center w-full h-full"
               >
-                <img
-                  className="max-h-32 max-w-[90%] object-contain"
-                  src={partner.imagePath}
-                  alt={partner.name}
-                />
+                <div className="flex items-center justify-center w-full h-full">
+                  <img
+                    className="max-h-32 max-w-[90%] object-contain"
+                    src={partner.imagePath}
+                    alt={partner.name}
+                  />
+                </div>
               </a>
             </div>
           </article>
-
-
-
-
         ))}
       </div>
     </div>
