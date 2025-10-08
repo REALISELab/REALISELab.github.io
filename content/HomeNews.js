6be54ae2829927f4ef6ec22d1ea39fc30453b4a0
Diego Costa <diegoelias1@gmail.com>
2025-10-07 23:34:22 -0400

    Comment out paper announcements from 2024
    Commented out several paper announcements in HomeNews.js.

--- content/HomeNews.js
+++ content/HomeNews.js
@@ -50,45 +50,45 @@ export const news = [
         description: 'January, 2025',
         // href: "", 
     },
-    {
-        title: 'Our paper "Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study" has been accepted at SANER 2025! ',
-        description: 'December, 2024',
-        href: "/blog/llama-saner-2025", 
-    },
-    {
-        title: 'Our paper "A Machine Learning-Based Approach For Detecting Malicious PyPI Packages" has been accepted at ACM SAC! Pre-print coming soon.',
-        description: 'November, 2024',
-        // href:"" 
-    },
-    {
-        title: 'Concordia has funded our application for ReAnimate 2025, the 2nd Summer School on Retro Gaming History, Critic, and Development.',
-        description: 'November, 2024',
-        href: "/blog/reanimate-2025" 
-    },
-    {
-        title: 'Our paper on "Predicting the First Response Latency of Maintainers and Contributors in Pull Requests" has been accepted at TSE!',
-        description: 'August, 2024',
-        href:"/blog/latency-tse-2025" 
-    },
-    {
-        title: 'Our paper on "Early Detection of Performance Regressions by Bridging Local Performance Data and Architectural Models" has been accepted at ICSE 2025!',
-        description: 'July, 2024',
-        href:"/blog/performance-icse-2025"
-    },
-    {
-        title: 'Our paper on "A Transformer-based Approach for Augmenting Software Engineering Chatbots Datasets" has been accepted at ESEM 2024!',
-        description: 'June, 2024',
-        href:"/blog/augmentation-esem-2024"
-    },
-    {
-        title: 'FRQNT has supported a 2-year reserach project to support open source library maintainers',
-        description: 'May, 2024',
-        href:"/blog/frqnt-project"    
-    },
-    {
-        title: 'Rachna received the Best Student Presentation Award at MSR for her presentation on the work “The role of library versions in Developer-ChatGPT conversations”. Congratulations Rachna!',
-        description: 'May, 2024',
-        href:"/blog/msr-data-challenge"
-    },
+    // {
+    //     title: 'Our paper "Exploring the Potential of Llama Models in Automated Code Refinement: A Replication Study" has been accepted at SANER 2025! ',
+    //     description: 'December, 2024',
+    //     href: "/blog/llama-saner-2025", 
+    // },
+    // {
+    //     title: 'Our paper "A Machine Learning-Based Approach For Detecting Malicious PyPI Packages" has been accepted at ACM SAC! Pre-print coming soon.',
+    //     description: 'November, 2024',
+    //     // href:"" 
+    // },
+    // {
+    //     title: 'Concordia has funded our application for ReAnimate 2025, the 2nd Summer School on Retro Gaming History, Critic, and Development.',
+    //     description: 'November, 2024',
+    //     href: "/blog/reanimate-2025" 
+    // },
+    // {
+    //     title: 'Our paper on "Predicting the First Response Latency of Maintainers and Contributors in Pull Requests" has been accepted at TSE!',
+    //     description: 'August, 2024',
+    //     href:"/blog/latency-tse-2025" 
+    // },
+    // {
+    //     title: 'Our paper on "Early Detection of Performance Regressions by Bridging Local Performance Data and Architectural Models" has been accepted at ICSE 2025!',
+    //     description: 'July, 2024',
+    //     href:"/blog/performance-icse-2025"
+    // },
+    // {
+    //     title: 'Our paper on "A Transformer-based Approach for Augmenting Software Engineering Chatbots Datasets" has been accepted at ESEM 2024!',
+    //     description: 'June, 2024',
+    //     href:"/blog/augmentation-esem-2024"
+    // },
+    // {
+    //     title: 'FRQNT has supported a 2-year reserach project to support open source library maintainers',
+    //     description: 'May, 2024',
+    //     href:"/blog/frqnt-project"    
+    // },
+    // {
+    //     title: 'Rachna received the Best Student Presentation Award at MSR for her presentation on the work “The role of library versions in Developer-ChatGPT conversations”. Congratulations Rachna!',
+    //     description: 'May, 2024',
+    //     href:"/blog/msr-data-challenge"
+    // },
     
 ]
